858e44e451606e1399da643a527c3b6a63e35f83
j23414 <jenchang@iastate.edu>
2015-12-30 12:36:56 -0600

    restructuring

--- pages/jpages/gallery/gencode.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# USAGE: ls *.jpg | bash gencode.sh
-
-while read -r line
-do
-    filename=$(basename $line .jpg)
-    echo "<li><a href=\"#\"><img src=\"images/thumbs/$line\" data-large=\"images/$line\" alt=\"image$filename\" data-description=\"Mango gallery image$filename\" /></a></li>"
-done
